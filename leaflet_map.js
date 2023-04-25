9c5e0ef5b10edd1cf0c5e55785f7eea9b0bbd24b
Choon Leong <musiziano@gmail.com>
2023-04-25 13:20:16 +0800

    dark map leaflet

--- leaflet_map.js
+++ leaflet_map.js
@@ -1,9 +1,21 @@
 // Initialise map to Singapore coordinate and define zoom level
-var map = L.map("map").setView([1.3615208221204578, 103.8160867611435], 12);
+let map = L.map("map").setView([1.3615208221204578, 103.8160867611435], 12);
 
 // Map Layer
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+
+let mapLayer = L.tileLayer(
+  "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+  {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+  }
+);
+
+// let mapLayer = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//   maxZoom: 19,
+//   attribution:
+//     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+// });
+
+mapLayer.addTo(map);
