11b7972554bb556e538155c5b1b838bb466fce8a
Choon Leong <musiziano@gmail.com>
2023-04-25 22:21:26 +0800

    add layer control

--- leaflet_map.js
+++ leaflet_map.js
@@ -1,9 +1,20 @@
 // Initialise map to Singapore coordinate and define zoom level
 let map = L.map("map").setView([1.3615208221204578, 103.8160867611435], 12);
 
-// Map Layer
+//todo: Toggle between colored and dark maps
 
-let mapLayer = L.tileLayer(
+// Open street layer
+let openStreetLayer = L.tileLayer(
+  "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+  {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }
+);
+
+// Dark map layer
+let darkMapLayer = L.tileLayer(
   "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
   {
     maxZoom: 19,
@@ -12,4 +23,28 @@ let mapLayer = L.tileLayer(
   }
 );
 
-mapLayer.addTo(map);
+// Google street layer
+let googleLayer = L.tileLayer(
+  "http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}",
+  {
+    maxZoom: 20,
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
+  }
+);
+
+darkMapLayer.addTo(map);
+
+let baseMaps = {
+  "Dark Map": darkMapLayer,
+  "Open Street Map": openStreetLayer,
+  "Google Streets": googleLayer,
+};
+
+// let overlayMaps = {
+//   Cities: cities,
+// };
+
+let layerControl = L.control.layers(baseMaps, null, {
+  collapsed: false,
+});
+layerControl.addTo(map);
